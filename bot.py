0db71555ec1cc107000ec9c1cad48d6f195c74d4
ishraq islam <21dcba12@gmail.com>
2022-02-26 17:58:38 +0600

    ready for pr :)

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 import discord
-
-channelID = 942764372164481094
+import tasks
 
 class CoolBot(discord.Client):
+    def __init__(self, intents):
+        super(CoolBot, self).__init__(intents = intents)
+        self.task = tasks.BotTasks(self)
+
     async def on_connect(self):
         print('[LOGS] Connecting to discord!')
 
@@ -18,7 +21,21 @@ class CoolBot(discord.Client):
         if message.author == self.user:
             return
 
-        if message.channel.id == channelID:
-            await message.add_reaction("<:pepeyessign:889089415174058026>")
-            await message.add_reaction("<:pepenosign:889089399072108584>")
+        # These are in self.task.send_problem()
+        # if message.channel.id == channelID:
+        #     await message.add_reaction("<:pepeyessign:889089415174058026>")
+        #     await message.add_reaction("<:pepenosign:889089399072108584>")
 
+        # New commands
+        if message.content.startswith('-cool'):
+            # Arguments "channel", "command", "reactions" for flexibilty
+            await self.task.send_problem(message, "cool-problems", "-cool", ["<:pepeyessign:889089415174058026>", "<:pepenosign:889089399072108584>"])
+    
+        # Same can be done for marathon problems
+        elif message.content.startswith('-marathon'):
+            await self.task.send_problem(message, "marathon-problems", '-marathon', ["<:plus_one:896423100953022504>", "<:minus_one:938471992976367676>"])
+
+        # Help command
+        elif message.content.startswith("-help"):
+            with open("help_msg.txt", encoding = "utf8") as f:
+                await message.channel.send(f.read())
